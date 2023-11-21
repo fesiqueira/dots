b1d9251be357a56024ada70b2e077dac59449f47
Felipe Pinheiro <fesiqueirapinheiro@gmail.com>
2023-11-20 22:33:20 -0300

    Refactor functions
    :D

--- dots.go
+++ dots.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 )
 
-var charMap = [4][2]int{
+const brailleOffset rune = 0x2800
+
+var charMap = [4][2]rune{
 	{1, 8},
 	{2, 16},
 	{4, 32},
@@ -15,56 +17,83 @@ var charMap = [4][2]int{
 type Canvas struct {
 	Width  int
 	Height int
-	chars  []int
+	width  int
+	height int
+	chars  []rune
 }
 
 func NewCanvas(width, height int) *Canvas {
-	w := int(math.Round(float64(width) / 2.0))
-	h := int(math.Round(float64(height) / 4.0))
-
-	chars := make([]int, w*h)
-	for i := range chars {
-		chars[i] = 0x2800
-	}
+	w := int(math.Ceil(float64(width) / 2))
+	h := int(math.Ceil(float64(height) / 4))
 
 	return &Canvas{
-		Width:  w,
-		Height: h,
-		chars:  chars,
+		Width:  width,
+		Height: height,
+		width:  w,
+		height: h,
+		chars:  make([]rune, w*h),
 	}
 }
 
-func blockCoord(x, y int) (int, int) {
-	return x / 2, y / 4
-}
-
-func mapChar(x, y int) int {
+func getChar(x, y int) rune {
 	return charMap[y%4][x%2]
 }
 
-func (c Canvas) convert(x, y int) int {
-	return x + y*c.Width
+func (c Canvas) getBlockIdx(x, y int) int {
+	return x + y*c.width
+}
+
+func (c Canvas) getIdx(x, y int) int {
+	return x/2 + y/4*c.width
+}
+
+func (c Canvas) hasPixel(x, y int) bool {
+	idx := c.getIdx(x, y)
+	return c.chars[idx]&getChar(x, y) > 0
+}
+
+func (c *Canvas) togglePixel(x, y int) {
+	if c.hasPixel(x, y) {
+		c.unsetPixel(x, y)
+		return
+	}
+	c.setPixel(x, y)
+}
+
+func (c *Canvas) unsetPixel(x, y int) {
+	idx := c.getIdx(x, y)
+	c.chars[idx] ^= getChar(x, y)
 }
 
 func (c *Canvas) setPixel(x, y int) {
-	bX, bY := blockCoord(x, y)
-	idx := c.convert(bX, bY)
+	idx := c.getIdx(x, y)
+	c.chars[idx] |= getChar(x, y)
+}
 
-	c.chars[idx] += mapChar(x, y)
+func (c Canvas) rows() []string {
+	rows := make([]string, c.height)
+
+	for y := 0; y < c.height; y++ {
+		row := make([]rune, c.width)
+		for x := 0; x < c.width; x++ {
+			blk := c.getBlockIdx(x, y)
+			row[x] = brailleOffset + c.chars[blk]
+		}
+		rows[y] = string(row)
+	}
+
+	return rows
 }
 
 func (c *Canvas) Clear() {
-	c.chars = make([]int, c.Width*c.Height)
+	c.chars = make([]rune, c.width*c.height)
 }
 
 func (c *Canvas) String() string {
 	var b strings.Builder
 
-	for y := 0; y < c.Height; y++ {
-		for x := 0; x < c.Width; x++ {
-			b.WriteString(string(rune(c.chars[c.convert(x, y)])))
-		}
-		b.WriteString("\n")
+	for _, row := range c.rows() {
+		b.WriteString(row + "\n")
 	}
 
 	return b.String()
--- dots_test.go
+++ dots_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 )
 
-
 func TestString(t *testing.T) {
 	c := NewCanvas(10, 10)
 
@@ -22,35 +21,35 @@ func TestString(t *testing.T) {
 	c.setPixel(7, 4)
 	c.setPixel(6, 3)
 	c.setPixel(5, 3)
-	c.setPixel(4, 4)
+	c.togglePixel(4, 4)
 	fmt.Printf("%s\n", c)
 }
 
-func TestPrintBlock(t*testing.T){
-c := NewCanvas(10,10)
+func TestPrintBlock(t *testing.T) {
+	c := NewCanvas(10, 10)
 
-c.setPixel(0,0)
-c.setPixel(1,0)
-c.setPixel(2,0)
-c.setPixel(3,0)
-c.setPixel(4,0)
-c.setPixel(5,0)
-c.setPixel(6,0)
-c.setPixel(7,0)
-c.setPixel(8,0)
-c.setPixel(9,0)
+	c.setPixel(0, 0)
+	c.setPixel(1, 0)
+	c.setPixel(2, 0)
+	c.setPixel(3, 0)
+	c.setPixel(4, 0)
+	c.setPixel(5, 0)
+	c.setPixel(6, 0)
+	c.setPixel(7, 0)
+	c.setPixel(8, 0)
+	c.setPixel(9, 0)
 
-c.setPixel(0,1)
-c.setPixel(0,2)
-c.setPixel(0,3)
-c.setPixel(0,4)
-c.setPixel(0,5)
-c.setPixel(0,6)
-c.setPixel(0,7)
-c.setPixel(0,8)
-c.setPixel(0,9)
+	c.setPixel(0, 1)
+	c.setPixel(0, 2)
+	c.setPixel(0, 3)
+	c.setPixel(0, 4)
+	c.setPixel(0, 5)
+	c.setPixel(0, 6)
+	c.setPixel(0, 7)
+	c.setPixel(0, 8)
+	c.setPixel(0, 9)
 
-c.setPixel(1,9)
+	c.setPixel(1, 9)
 
-fmt.Printf("%s\n", c)
+	fmt.Printf("%s\n", c)
 }
